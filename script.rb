d116a56e1063a3e73488f68b4b95d998a2444c6f
Caleb Smith <karagenit@outlook.com>
2017-09-16 23:28:26 +0000

    Compute Min/Max/Mean

--- script.rb
+++ script.rb
@@ -3,6 +3,7 @@
 min = 15
 max = 15
 total = 0
+count = 0
 
 Dir.foreach('./bestiary/_posts/') do |path|
   next if path == '.' or path == '..'
@@ -12,8 +13,22 @@ Dir.foreach('./bestiary/_posts/') do |path|
   file = File.open(path, 'r')
   file.each_line do |line|
     if line.include? "Armor Class"
-      puts line[/([0-9]{1,2})/, 1]
+      ac = line[/([0-9]{1,2})/, 1].to_i
+      total += ac
+      count += 1
+      if ac < min
+        min = ac
+      end
+      if ac > max
+        max = ac
+      end
     end
   end
   file.close
 end
+
+mean = total / count
+
+puts "Maximum: " + max.to_s
+puts "Minimum: " + min.to_s
+puts "Mean: " + mean.to_s
