9d3db427f9612ae0d98ec7ebadc3ec5d142939c0
Caleb Smith <karagenit@outlook.com>
2017-09-17 03:50:44 +0000

    Add Array Graphing

--- script.rb
+++ script.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
 
+def graphArray(array)
+  array.each_with_index do |count, ac|
+    printf "%02d|%s\n", ac, ('#' * count)
+  end
+end
+
 min = 15
 max = 15
 total = 0
 count = 0
+acs = Array.new(30, 0)
 
 Dir.foreach('./bestiary/_posts/') do |path|
   next if path == '.' or path == '..'
@@ -16,6 +23,7 @@ Dir.foreach('./bestiary/_posts/') do |path|
       ac = line[/([0-9]{1,2})/, 1].to_i
       total += ac
       count += 1
+      acs[ac] += 1
       if ac < min
         min = ac
       end
@@ -29,6 +37,8 @@ end
 
 mean = total / count
 
+puts "Counted: " + count.to_s
 puts "Maximum: " + max.to_s
 puts "Minimum: " + min.to_s
 puts "Mean: " + mean.to_s
+graphArray(acs)
